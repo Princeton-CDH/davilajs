e1d2fa098a62100bbaade2eeec2171b1ef502bcf
rlskoeser <rebecca.s.koeser@princeton.edu>
2017-12-01 16:35:29 -0500

    Preliminary jekyll site with prototype davila code and tests

--- README.md
+++ README.md
@@ -4,9 +4,29 @@ Interactive, Iterative Schema Documentation
 
 davila.js will be a complete reimplementation of [DAVILA](https://github.com/jabauer/DAVILA/), an interactive schema annotation tool, built in [Processing](https://processing.org/) and released in 2010.  The new version will be a browser-based tool with interactive customization features.
 
-This repo uses `git-flow <https://github.com/nvie/gitflow>`_ conventions; **master**
+This repo uses [git-flow](https://github.com/nvie/gitflow) conventions; **master**
 contains the most recent release, and work in progress will be on the **develop** branch.
 Pull requests should be made against develop.
+
+
+# Development instructions
+
+## Initial setup and installation
+
+* Install [jekyll](https://jekyllrb.com/docs/installation/), which is currently
+used for running davila.js and unit tests in the browser: `gem install jekyll`
+
+* Install npm and dependencies for unit testing: `npm install`
+
+* Run jekyll: `jekyll serve`
+
+## Unit tests
+
+Tests are written with mocha and chai and currently run through the browser.
+To run them, start jekyll and navigate to http://localhost:4000/test/
+
+
+
 
 
 
---
+++ src/parse.js
@@ -0,0 +1,44 @@
+var tablename_re = /CREATE\sTABLE\s`(\w+)`[^;]*;/g;
+// var foreign_key_re = /CREATE\sTABLE\s`(\w+)`[^;]*FOREIGN\sKEY\s\(`(\w+)`\)\sREFERENCES\s`(\w+)`/gm;
+var foreign_key_re = /FOREIGN\sKEY\s\(`(\w+)`\)\sREFERENCES\s`(\w+)`/g;
+
+function parse(schema) {
+    // parse mysql schema
+    var entities = [];
+    var relationships = []
+    var match, keymatch;
+    // look for table names and generate a list of entities
+    while ((match = tablename_re.exec(schema)) !== null) {
+        var table = match[1],
+            table_details = match[0];
+        entities.push({'id': table});
+
+        // todo: gather field details for the table
+
+        // look for foreign keys within the table definition
+        while ((keymatch = foreign_key_re.exec(table_details)) !== null) {
+            relationships.push({'source': table, 'target': keymatch[2], 'value': 1})
+        }
+
+    }
+
+/*    while ((match = foreign_key_re.exec(schema)) !== null) {
+        console.log(match);
+        relationships.push({'source': match[1], 'target': match[3]})
+    }
+*/
+    console.log(relationships);
+
+  // "relationships": [
+    // {"source": "People", "target": "Relationships", "value": 1},
+    // {"source": "People", "target": "Nationalities", "value": 1},
+
+    // console.log(entities);
+    return {
+        'entities': entities,
+        'relationships': relationships
+    };
+}
+
+
+
