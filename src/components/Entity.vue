a3490752fc647578d6e376aea305e90116bc5fc3
rlskoeser <rebecca.s.koeser@princeton.edu>
2018-10-24 15:21:52 -0400

    Add more details & styles to entity component based on previous version

--- src/components/Entity.vue
+++ src/components/Entity.vue
@@ -1,7 +1,12 @@
 <template>
     <div class="entity">
-        <span>id: {{ id }}</span>
-        <div>{{ fields }}</div>
+        <h2>{{ id }}</h2>
+        <ul class="fields">
+            <li v-for="field in fields">
+                {{ field.name }}
+                <span class="type">{{ field.type }}</span>
+            </li>
+        </ul>
     </div>
 </template>
 
@@ -14,9 +19,31 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
 .entity {
     display: inline-block;
-    width: 200px;
+    background-color: white;
+    min-width: 150px;
+    padding: 10px;
+    margin: 2px;
+    position: absolute;
+    border-style: solid;
+    z-index: 4;
+
+    h2 {
+        padding-right: 2em;
+
+    }
+
+   .fields {
+      // display:none;  /* for now; toggleable later */
+      list-style-type: none;
+      padding-left: 0;
+
+      .type {
+          float: right;
+          margin-left: 10px;
+      }
+    }
 }
 </style>
