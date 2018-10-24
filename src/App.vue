e98f7f38845aa561d58d6eae6b8de43d0cd3cc54
Nick Budak <thatbudakguy@gmail.com>
2018-10-24 14:48:04 -0400

    create DropZone component

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,24 @@
 <template>
-  <div id="app" @drop="onDrop" @dragover="onDragOver">
+  <div class="app">
+    <DropZone/>
   </div>
 </template>
 
 <script>
-import { mysql } from './parser'
+import DropZone from './components/DropZone'
 
 export default {
-  name: 'app',
-  data() {
-    return {
-      reader: Object,
-    }
-  },
-  created() {
-    this.reader = new FileReader()
-    this.reader.onload = event => this.parseSchema(event.target.result)
-  },
-  methods: {
-    onDrop(ev) {
-      ev.preventDefault()
-      if (ev.dataTransfer.items) {
-        // Use DataTransferItemList interface to access the file(s)
-        for (var i = 0; i < ev.dataTransfer.items.length; i++) {
-          // If dropped items aren't files, reject them
-          if (ev.dataTransfer.items[i].kind === 'file') {
-            this.reader.readAsText(ev.dataTransfer.items[i].getAsFile())
-          }
-        }
-      } else {
-        // Use DataTransfer interface to access the file(s)
-        for (var i = 0; i < ev.dataTransfer.files.length; i++) {
-          this.reader.readAsText(ev.dataTransfer.files[i])
-        }
-      }
-    },
-    onDragOver(event) {
-      event.preventDefault()
-    },
-    parseSchema(schema) {
-      console.log(mysql.parse(schema))
-    }
-  },
+  components: {
+    DropZone
+  }
 }
 </script>
 
 <style>
-#app {
+.app {
   width: 100%;
   height: 100%;
   position: absolute;
-  background-color: red;
+  background-color: white;
 }
 </style>
--- src/components/DropZone.vue
+++ src/components/DropZone.vue
@@ -1,8 +1,53 @@
 <template>
+    <div class="dropzone" @drop="onDrop" @dragover="onDragOver"></div>
 </template>
 
 <script>
+import { mysql } from '../parser'
+
+export default {
+  data() {
+    return {
+      reader: Object,
+    }
+  },
+  created() {
+    this.reader = new FileReader()
+    this.reader.onload = event => this.parseSchema(event.target.result)
+  },
+  methods: {
+    onDrop(ev) {
+      ev.preventDefault()
+      if (ev.dataTransfer.items) {
+        // Use DataTransferItemList interface to access the file(s)
+        for (var i = 0; i < ev.dataTransfer.items.length; i++) {
+          // If dropped items aren't files, reject them
+          if (ev.dataTransfer.items[i].kind === 'file') {
+            this.reader.readAsText(ev.dataTransfer.items[i].getAsFile())
+          }
+        }
+      } else {
+        // Use DataTransfer interface to access the file(s)
+        for (var i = 0; i < ev.dataTransfer.files.length; i++) {
+          this.reader.readAsText(ev.dataTransfer.files[i])
+        }
+      }
+    },
+    onDragOver(event) {
+      event.preventDefault()
+    },
+    parseSchema(schema) {
+      console.log(mysql.parse(schema))
+    }
+  },
+}
 </script>
 
 <style>
+.dropzone {
+  width: 20%;
+  height: 100%;
+  position: absolute;
+  background-color: red;
+}
 </style>
